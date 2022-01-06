6ca19759f565c2da48694ab06a69d24cefba1eb5
Owner <Owner@DESKTOP-5BCBMPS>
2022-01-06 20:04:13 +0900

    二回目のコミット

---
+++ src/java/beans/ArrayListEntity.java
@@ -0,0 +1,39 @@
+package beans;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ArrayListEntity implements Serializable {
+    @Id
+    private String id;
+    private String name;
+    @ElementCollection
+    @CollectionTable(name="course")
+    private List<String> course;
+
+    public ArrayListEntity () {}
+    
+    public ArrayListEntity (String id, String name, List<String> course) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getCourse() {
+        return course;
+    }
+}
+
